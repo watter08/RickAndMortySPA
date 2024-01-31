0084712239e14d06f699584d12afcadf2d626a2b
watter08 <watter08@hotmail.es>
2024-01-31 09:51:43 -0400

    Modalcomponent al 50%

---
+++ src/components/Home/RickAndMortyModal.vue
@@ -0,0 +1,26 @@
+<template>
+    <ModalComponent Title="Character Details" :ModalId="Character?.Name">
+        <h3>Hello World</h3>
+    </ModalComponent>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ModalComponent from '../share/ModalComponent.vue';
+import type { Character } from '@/interface/types/RickAndMorty/CharacterTypes';
+export default defineComponent({
+    name:'RickAndMortyModal',
+    components: {
+        ModalComponent
+    },
+    props:{
+        Character: {
+            type: Object,
+            default:{} as Character
+        },
+        ModalId: {
+            type: String,
+            default:'' as String
+        }
+    }
+})
+</script>
---
+++ src/components/share/ModalComponent.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="modal fade" :id="ModalId" tabindex="-1" :aria-labelledby="ModalId+'lLabel'" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" :id="ModalId+'lLabel'">{{ Title }}</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <slot />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name:'ModalComponent',
+    props:{
+        ModalId:{
+            type:String,
+            default:'exampleModal' as String
+        },
+        Title:{
+            type:String,
+            default:'' as String
+        }
+    }
+})
+</script>
